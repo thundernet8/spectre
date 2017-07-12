c955cc7551ddbc7cbc99c1bd7c6418a37caf415c
wuxueqian <wuxueqian2010@hotmail.com>
2017-07-13 00:44:21 +0800

    Init tools

---
+++ webpack.config.babel.js
@@ -0,0 +1,69 @@
+import path from "path";
+import webpack from "webpack";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
+import OptimizeCssAssetsPlugin from "optimize-css-assets-webpack-plugin";
+
+const genConfig = minimal => ({
+    node: {
+        __filename: false,
+        __dirname: false
+    },
+    entry: {
+        spectre: "./components/index.tsx"
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: minimal ? "[name].min.js" : "[name].js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx"]
+    },
+    target: "web",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/node_modules/],
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!sass-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new ExtractTextPlugin({
+            filename: minimal ? "spectre.min.css" : "spectre.css",
+            disable: false,
+            allChunks: true
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.min\.css$/g,
+            cssProcessor: require("cssnano"),
+            cssProcessorOptions: { discardComments: { removeAll: true } },
+            canPrint: true
+        })
+    ]
+});
+
+export default [genConfig(false), genConfig(true)];
